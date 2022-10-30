fe447d431b460faf35c0cabc1336f3d170e8affb
nfebrian13 <nanaz.febrian@gmail.com>
2022-10-30 10:20:43 +0700

    add crud features in learn django web

--- members/views.py
+++ members/views.py
@@ -1,9 +1,67 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
+from .models import Members
 
 
 def index(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+
+def addrecord(request):
+    x = request.POST['first']
+    y = request.POST['last']
+    member = Members(firstname=x, lastname=y)
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def delete(request, id):
+    member = Members.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def update(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    member = Members.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+def index4(request):
+    mymembers = Members.objects.all().values()
+    output = ""
+    for x in mymembers:
+        output += x["firstname"]
+    return HttpResponse(output)
+
+
+def index3(request):
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
 
